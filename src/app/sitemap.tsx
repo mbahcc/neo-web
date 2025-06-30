1fad3d4502583f09807b1d3b12277f4436c092fd
Collins Mbah <mbahcc@mail.uc.edu>
2025-06-29 23:18:04 -0700

    added sitemap

---
+++ src/app/sitemap.tsx
@@ -0,0 +1,25 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://neoinitativeuc.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://neoinitativeuc.com/events",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://neoinitativeuc.com/getinvolved",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://neoinitativeuc.com/events",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://neoinitativeuc.com/aboutus",
+      lastModified: new Date(),
+    },
+    // Add all other pages
+  ];
+}
