b44542ca256ace2231a4420bdc8c1ef2b389bcd8
Collins Mbah <mbahcc@mail.uc.edu>
2025-07-13 22:56:09 -0700

    fixed spelling errors

--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -1,23 +1,23 @@
 export default function sitemap() {
   return [
     {
-      url: "https://neoinitativeuc.com",
+      url: "https://neoinitiativeuc.com",
       lastModified: new Date(),
     },
     {
-      url: "https://neoinitativeuc.com/events",
+      url: "https://neoinitiativeuc.com/events",
       lastModified: new Date(),
     },
     {
-      url: "https://neoinitativeuc.com/getinvolved",
+      url: "https://neoinitiativeuc.com/getinvolved",
       lastModified: new Date(),
     },
     {
-      url: "https://neoinitativeuc.com/events",
+      url: "https://neoinitiativeuc.com/events",
       lastModified: new Date(),
     },
     {
-      url: "https://neoinitativeuc.com/aboutus",
+      url: "https://neoinitiativeuc.com/aboutus",
       lastModified: new Date(),
     },
     // Add all other pages
